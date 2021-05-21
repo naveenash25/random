aeace2d3dc8db02e88d58ef72c046cc04d3371bf
Naveen Satti <naveen.satti@dxc.com>
2021-05-22 01:22:03 +0530

    change variable name

--- function-sort.js
+++ function-sort.js
@@ -2,8 +2,8 @@
 const given = [2, 12, 5, 7, 1, 4]
 var arguments = process.argv
 const order = arguments[2] || 'asc';
-const maxNumber = given => given.reduce((a, b) => a > b ? a : b)
-const minNumber = given => given.reduce((a, b) => a < b ? a : b)
+const maxNumber = _ => _.reduce((a, b) => a > b ? a : b)
+const minNumber = _ => _.reduce((a, b) => a < b ? a : b)
 let givenArray = given;
 const sortedArray = given.map((_) => {
     let max = order == 'asc' ? maxNumber(givenArray) : minNumber(givenArray)
